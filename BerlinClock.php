ab432e8cee306c138741211ffd5a59f2336baea3
Gerald Crisostomo <gerald.crisostomo@student.vinci.be>
2022-01-04 18:04:31 +0100

    Add testSimpleMinutesC3 and Refactor function simpleMinutes

--- BerlinClock.php
+++ BerlinClock.php
@@ -3,7 +3,8 @@
 class BerlinClock
 {
     public function simpleMinutes(int $minute):string{
-        if($minute == 1 ||$minute%5==1) return "0001";
+        if($minute == 1 ||$minute%5==1) return "1000";
+        if($minute == 2 ||$minute%5==2) return "1100";
         return "0000";
     }
 }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -14,6 +14,14 @@ class BerlinClockTest extends TestCase
     public function testSimpleMinutesC2(){
         $minute = new BerlinClock();
         $actual= $minute->simpleMinutes(1);
-        $this->assertEquals('0001', $actual);
+        $this->assertEquals('1000', $actual);
     }
+
+    public function testSimpleMinutesC3(){
+        $minute = new BerlinClock();
+        $actual= $minute->simpleMinutes(7);
+        $this->assertEquals('1100', $actual);
+    }
+
+
 }
